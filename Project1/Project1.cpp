e0f13dd7337cdc0cb2f129a8387b4e1dbd831bd4
Pat Tul <patchatulyakul@gmail.com>
2026-09-12 14:11:59 -0400

    init project1

---
+++ Project1/Project1.cpp
@@ -0,0 +1,15 @@
+/*
+COP4530 Project 1 Circularly Linked List
+Our project is about implementing a circularly linked list in Cafe Menu.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// main function to run the program
+int main(){
+    cout << "Hello World!" << endl;
+    return 0;
+}
